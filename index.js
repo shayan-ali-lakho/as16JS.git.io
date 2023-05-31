750f4cc631493dae4b119d2c46afbb2c4992ec10
shayan-ali-lakho <shayanlakho25@gmail.com>
2023-05-31 20:31:10 +0500

    initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+let cardsnum = prompt("How Much card do you want");
+let arr=[];
+for( let i=0; i<cardsnum; i++)
+        {
+     let m = prompt("Model Name");
+    let d = prompt("D");
+      let i = prompt("forme3.jpg");
+    // var main=;
+      
+arr.push({m,d,i});
+    }
+   
